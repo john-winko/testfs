d9a10e52bad6d5ebe41cdcb30dda42ada37e71c9
john-winko <john.winko@gmail.com>
2022-04-12 11:15:39 -0500

    tweaed

--- app/views.py
+++ app/views.py
@@ -6,4 +6,5 @@ from django.http import HttpResponse
 # Create your views here.
 def send_the_homepage(request):
     theIndex = open('build/index.html').read()
+    print(theIndex)
     return HttpResponse(theIndex)
