5303e1d736adab0d3e3150b2638245743bbfb92e
john-winko <john.winko@gmail.com>
2022-04-12 10:02:31 -0500

    init

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+# Create your views here.
+def send_the_homepage(request):
+    theIndex = open('build/index.html').read()
+    return HttpResponse(theIndex)
